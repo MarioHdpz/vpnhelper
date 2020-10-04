c04aeaf4857bdd81449a212f2a8f75e553d9bf56
Mario <mariohdpz@gmail.com>
2020-10-04 02:45:22 -0500

    Format code according to black default format

--- freevpn.py
+++ freevpn.py
@@ -3,12 +3,13 @@ import requests
 from bs4 import BeautifulSoup
 
 
-__author__ = "wrathdev GGSIPUResultTracker orekihotaro demonnezuko varmaincorp hotarooreki nezukodemon"
+__author__ = ("wrathdev GGSIPUResultTracker orekihotaro "
+              "demonnezuko varmaincorp hotarooreki nezukodemon")
 
-RE_PASS = re.compile(r'(<li><strong>Password:<\/strong>)(?P<password>[^<]+)(<\/li>)')
+RE_PASS = re.compile(r"(<li><strong>Password:<\/strong>)(?P<password>[^<]+)(<\/li>)")
 
-TEXT_ONLINE = 'Online'
-VPNS = ('me', 'eu', 'se', 'im', 'it', 'be')
+TEXT_ONLINE = "Online"
+VPNS = ("me", "eu", "se", "im", "it", "be")
 
 
 def get_password(username):
@@ -22,16 +23,17 @@ def get_password(username):
                         freevpn.se
     """
     passw = None
-    html  = requests.get('https://'+ username + '/accounts').text
+    html = requests.get("https://" + username + "/accounts").text
 
     match = RE_PASS.search(html)
     if match:
         passw = match.groups()[1]
-    
+
     return passw.strip() if passw else None
 
+
 def get_vpn_status():
-     """
+    """
     Get the status of FreeVPN Servers
     It scrapes the FreeVPN website for server status and details.
 
@@ -40,21 +42,22 @@ def get_vpn_status():
         Example dict:-
             {'name':'freevpn.me', 'loc':'France', 'online':true, 'load':'9'}
     """
-    html = requests.get('https://freevpn.me/').text
-    soup = BeautifulSoup(html, 'lxml')
+    html = requests.get("https://freevpn.me/").text
+    soup = BeautifulSoup(html, "lxml")
 
     table = soup.find_all(
-        'table', attrs={'class': "table table-striped table-bordered dataTable"})
+        "table", attrs={"class": "table table-striped table-bordered dataTable"}
+    )
     if table:
         table = table[0]
         tbody = table.tbody
-        trows = tbody.find_all('tr')[1:]
+        trows = tbody.find_all("tr")
         for row in trows:
             name = None
             loc = None
             online = False
             load = None
-            tcols = row.find_all('td')
+            tcols = row.find_all("td")
             for i, tcol in enumerate(tcols):
                 if i == 1:
                     name = tcol.text.strip().lower()
@@ -66,9 +69,4 @@ def get_vpn_status():
                 elif i == 8:
                     load = tcol.text.strip()[:-1]
             if name:
-                yield {'name': name, 'loc': loc,
-                       'online': online, 'load': load}
-
-
-
-
+                yield {"name": name, "loc": loc, "online": online, "load": load}
